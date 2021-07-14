d9041983a9f69f8d9e3ac681c271209b28e34ba3
Muhammad Irtiza <muhammad.irtiza@brainxtech.com>
2021-07-14 14:12:04 +0500

    Created the xcode project.

---
+++ Reminder-App/Reminder-App/Reminder.swift
@@ -0,0 +1,37 @@
+//
+//  Reminder.swift
+//  Reminder-App
+//
+//  Created by BrainX Technologies 11 on 14/07/2021.
+//
+
+import Foundation
+
+struct Reminder {
+    var title: String
+    var dueDate: Date
+    var notes: String? = nil
+    var isComplete: Bool = false
+}
+
+extension Reminder {
+    static var testData: [Reminder] = [
+        Reminder(title: "Create remote repo", dueDate: Date().addingTimeInterval(800.0), notes: "Use your own github account"),
+        
+        Reminder(title: "Clone it to local machine", dueDate: Date().addingTimeInterval(14000.0), notes: "Use git clone command"),
+        
+        Reminder(title: "Create Xocde project", dueDate: Date().addingTimeInterval(24000.0), notes: "This project should be in master branch"),
+        
+        Reminder(title: "Commit and push", dueDate: Date().addingTimeInterval(3200.0), notes: "Good commit message"),
+        
+        Reminder(title: "Create branch for feature", dueDate: Date().addingTimeInterval(60000.0), notes: "Call this branch feature/feature-name", isComplete: ture),
+        
+        Reminder(title: "Implement the feature", dueDate: Date().addingTimeInterval(72000.0), notes: "Write clean code"),
+        
+        Reminder(title: "Create PR when feature is complete", dueDate: Date().addingTimeInterval(80000.0), notes: "blah blah"),
+        
+        Reminder(title: "Review the PR", dueDate: Date().addingTimeInterval(92000.0), notes: "Arslan will review it", isComplete: true),
+        
+        Reminder(title: "Merge the commit", dueDate: Date().addingTimeInterval(101000.0), notes: "Good commit message")
+    ]
+}
---
+++ Reminder-App/Reminder-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder-App
+//
+//  Created by BrainX Technologies 11 on 14/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
